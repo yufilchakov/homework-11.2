593e223038abf8b3b59098188623772b7f9d09b6
yura_filchakov <123253377+yufilchakov@users.noreply.github.com>
2024-03-24 17:18:50 +0700

    add uppercase_string

--- str_func.py
+++ str_func.py
@@ -1,5 +1,5 @@
 def uppercase_string(input_string):
-    return input_string.upper()
 """
 Функция принимает на вход строку и возвращает ее со всеми заглавными буквами.
 """
+    return input_string.upper()
