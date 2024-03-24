10d62a1e5b7ec9b9d33596d18afffaa540d50c33
Yura Filchakov <filchakov1993@gmail.com>
2024-03-24 17:11:14 +0700

    add capitalize_words

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@ def uppercase_string(input_string):
 """
 Функция принимает на вход строку и возвращает ее со всеми заглавными буквами.
 """
+
+def capitalize_words(input_string):
+    """
+    Функция принимает на вход строку и возвращает ее со всеми заглавными буквами.
+    """
+    return ' '.join(word.capitalize() for word in input_string.split())
