749ae4fea2b947071a0da8ec252fcfddae63a022
Yura Filchakov <filchakov1993@gmail.com>
2024-03-24 18:02:48 +0700

    adddd str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def uppercase_string(input_string):
     return input_string.upper()
 
+
+def capitalize_words(input_string):
+    """
+    Функция принимает на вход строку и возвращает ее со всеми заглавными буквами.
+    """
+    return ' '.join(word.capitalize() for word in input_string.split())
